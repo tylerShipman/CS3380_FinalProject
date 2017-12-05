c60249e0e4dfedddebc4460177a0b18f083b9bac
Harrison <31551456+HarrisonLara@users.noreply.github.com>
2017-12-05 16:11:39 -0600

    create all player tables

--- 3380FinalProject/FinalProject/portfolio.php
+++ 3380FinalProject/FinalProject/portfolio.php
@@ -64,8 +64,43 @@
                             echo "</table>";
                 ?>
             </div>
+             <div class="tables">
+                <?php
+                    echo "<table border ='1' style='border-collapse: collapse'>";
+
+                    echo "<th> Home  </th>";
+                    echo "<th> Away  </th>";
+                    echo "<th> Date  </th>";
+
+                    for ($row=1; $row <= 3; $row++) { 
+                            echo "<tr>";
+                            for ($col=1; $col <= 3; $col++) { 
+                               echo "<td>Team $col</td>";
+                                }
+                             echo "</tr>";
+                            }
+                            echo "</table>";
+                ?>
+            </div>
+             <div class="tables">
+                <?php
+                    echo "<table border ='1' style='border-collapse: collapse'>";
+
+                    echo "<th> Home  </th>";
+                    echo "<th> Away  </th>";
+                    echo "<th> Date  </th>";
+
+                    for ($row=1; $row <= 3; $row++) { 
+                            echo "<tr>";
+                            for ($col=1; $col <= 3; $col++) { 
+                               echo "<td>Team $col</td>";
+                                }
+                             echo "</tr>";
+                            }
+                            echo "</table>";
+                ?>
+            </div>
         </div>
-        
     </body>
     <script type="text/javascript" src="js/bootstrap.min.js"></script>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
@@ -73,5 +108,5 @@
     <script type="text/javascript" src="form.js"></script>
     <script type="text/javascript" src="portfolio.js"></script>
     <script type="text/javascript" src="source/jquery.fancybox.js"></script>
-
+    
 </html>
