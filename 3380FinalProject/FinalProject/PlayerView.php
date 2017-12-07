84648c3133512443a71a8d76d7571b36fd3167e6
PharaohAlex <ajdvg9@mail.missouri.edu>
2017-12-06 18:45:46 -0600

    edits playerview

--- 3380FinalProject/FinalProject/PlayerView.php
+++ 3380FinalProject/FinalProject/PlayerView.php
@@ -1,226 +1,174 @@
 <?php
 
-	class TasksModel {
-		private $error = '';
-		private $mysqli;
-		private $orderBy = 'title';
-		private $orderDirection = 'asc';
+	class PlayerView{
+		private $stylesheet = 'PlayerCSS.css';
+		private $pageTitle = 'Players';
 		
 		public function __construct() {
-			session_start();
-			$this->initDatabaseConnection();
-			$this->restoreOrdering();
+
 		}
 		
 		public function __destruct() {
-			if ($this->mysqli) {
-				$this->mysqli->close();
+
+		}
+		
+		public function PlayerListView($players, $orderBy = 'firstName', $orderDirection = 'asc', $message = '') {
+			$body = "<h1>Players</h1>\n";
+		
+			if ($message) {
+				$body .= "<p class='message'>$message</p>\n";
 			}
-		}
 		
-		public function getError() {
-			return $this->error;
-		}
-		
-		private function initDatabaseConnection() {
-			require('db_credentials.php');
-			$this->mysqli = new mysqli($servername, $username, $password, $dbname);
-			if ($this->mysqli->connect_error) {
-				$this->error = $mysqli->connect_error;
-			}
-		}
-		
-		private function restoreOrdering() {
-			$this->orderBy = $_SESSION['orderby'] ? $_SESSION['orderby'] : $this->orderBy;
-			$this->orderDirection = $_SESSION['orderdirection'] ? $_SESSION['orderdirection'] : $this->orderDirection;
-		
-			$_SESSION['orderby'] = $this->orderBy;
-			$_SESSION['orderdirection'] = $this->orderDirection;
-		}
+			$body .= "<p><a class='playerButton' href='index.php?view=playerform'>+ Add player</a></p>\n";
 	
-		public function toggleOrder($orderBy) {
-			if ($this->orderBy == $orderBy)	{
-				if ($this->orderDirection == 'asc') {
-					$this->orderDirection = 'desc';
-				} else {
-					$this->orderDirection = 'asc';
-				}
-			} else {
-				$this->orderDirection = 'asc';
+			if (count($players) < 1) {
+				$body .= "<p>No players to display!</p>\n";
+				return $body;
 			}
-			$this->orderBy = $orderBy;
-			
-			$_SESSION['orderby'] = $this->orderBy;
-			$_SESSION['orderdirection'] = $this->orderDirection;			
-		}
+	
+			$body .= "<table>\n";
+			//$body .= "<tr><th>delete</th><th>edit</th><th>completed</th>";
+			$body .= "<tr><th>delete</th><th>edit</th>";
 		
-		public function getOrdering() {
-			return array($this->orderBy, $this->orderDirection);
-		}
+			$columns = array(array('name' => 'playerFirstName', 'label' => 'First Name'), 
+							 array('name' => 'PlayerLastName', 'label' => 'Last Name'), 
+							 array('name' => 'playerPosition', 'label' => 'Position'), 
+							 array('name' => 'playerNumber', 'label' => 'Number'), 
+							 array('name' => 'teamSchool', 'label' => 'School'));
 		
-		public function getTasks() {
-			$this->error = '';
-			$tasks = array();
-		
-			if (! $this->mysqli) {
-				$this->error = "No connection to database.";
-				return array($tasks, $this->error);
-			}
-		
-			$orderByEscaped = $this->mysqli->real_escape_string($this->orderBy);
-			$orderDirectionEscaped = $this->mysqli->real_escape_string($this->orderDirection);
-			$sql = "SELECT * FROM tasks ORDER BY $orderByEscaped $orderDirectionEscaped";
-			if ($result = $this->mysqli->query($sql)) {
-				if ($result->num_rows > 0) {
-					while($row = $result->fetch_assoc()) {
-						array_push($tasks, $row);
+			// geometric shapes in unicode
+			// http://jrgraphix.net/r/Unicode/25A0-25FF
+			foreach ($columns as $column) {
+				$name = $column['name'];
+				$label = $column['label'];
+				if ($name == $orderBy) {
+					if ($orderDirection == 'asc') {
+						$label .= " &#x25BC;";  // ▼
+					} else {
+						$label .= " &#x25B2;";  // ▲
 					}
 				}
-				$result->close();
+				$body .= "<th><a class='order' href='index.php?orderby=$name'>$label</a></th>";
+			}
+	
+			foreach ($players as $player) {
+				// $id = $task['id'];
+				// $addDate = $task['addDate'];
+				// $completedDate = ($task['completedDate']) ? $task['completedDate'] : '';
+				// $title = $task['title'];
+				// $description = ($task['description']) ? $task['description'] : '';
+				// $category = $task['category'];
+
+				$id = $player['player_id'];
+				$firstName = $player['playerFirstName'];
+				$lastName = ($player['playerLastName'];
+				$position = $player['playerPosition'];
+				$number = $player['playerNumber'];
+				$school = $player['teamSchool'];
+
+				// $completedAction = 'set_completed';
+				// $completedLabel = 'not completed';
+				// if ($completedDate) {
+				// 	$completedAction = 'set_not_completed';
+				// 	$completedLabel = 'completed';
+				// }
+			
+				$body .= "<tr>";
+				$body .= "<td><form action='index.php' method='post'><input type='hidden' name='action' value='delete' /><input type='hidden' name='id' value='$id' /><input type='submit' value='Delete'></form></td>";
+				$body .= "<td><form action='index.php' method='post'><input type='hidden' name='action' value='edit' /><input type='hidden' name='id' value='$id' /><input type='submit' value='Edit'></form></td>";
+				// $body .= "<td><form action='index.php' method='post'><input type='hidden' name='action' value='$completedAction' /><input type='hidden' name='id' value='$id' /><input type='submit' value='$completedLabel'></form></td>";
+				$body .= "<td>$firstName</td><td>$lastName</td><td>$position</td><td>$number</td><td>$school</td>";
+				$body .= "</tr>\n";
+			}
+			$body .= "</table>\n";
+	
+			return $this->page($body);
+		}
+		
+		public function taskFormView($data = null, $message = '') {
+			$category = '';
+			$title = '';
+			$description = '';
+			$selected = array('personal' => '', 'school' => '', 'work' => '', 'uncategorized' => '');
+			if ($data) {
+				$category = $data['category'] ? $data['category'] : 'uncategorized';
+				$title = $data['title'];
+				$description = $data['description'];
+				$selected[$category] = 'selected';
 			} else {
-				$this->error = $mysqli->error;
+				$selected['uncategorized'] = 'selected';
 			}
-			
-			return array($tasks, $this->error);
-		}
-		
-		public function getTask($id) {
-			$this->error = '';
-			$task = null;
-		
-			if (! $this->mysqli) {
-				$this->error = "No connection to database.";
-				return array($task, $this->error);
+	
+			$html = <<<EOT1
+<!DOCTYPE html>
+<html>
+<head>
+<title>Task Manager</title>
+<link rel="stylesheet" type="text/css" href="taskmanager.css">
+</head>
+<body>
+<h1>Tasks</h1>
+EOT1;
+
+			if ($message) {
+				$html .= "<p class='message'>$message</p>\n";
 			}
-			
-			if (! $id) {
-				$this->error = "No id specified for task to retrieve.";
-				return array($task, $this->error);
-			}
-			
-			$idEscaped = $this->mysqli->real_escape_string($id);
 		
-			$sql = "SELECT * FROM tasks WHERE id = '$idEscaped'";
-			if ($result = $this->mysqli->query($sql)) {
-				if ($result->num_rows > 0) {
-					$task = $result->fetch_assoc();
-				}
-				$result->close();
+			$html .= "<form action='index.php' method='post'>";
+		
+			if ($data['id']) {
+				$html .= "<input type='hidden' name='action' value='update' />";
+				$html .= "<input type='hidden' name='id' value='{$data['id']}' />";
 			} else {
-				$this->error = $this->mysqli->error;
+				$html .= "<input type='hidden' name='action' value='add' />";
 			}
-			
-			return array($task, $this->error);		
+		
+			$html .= <<<EOT2
+  <p>Category<br />
+  <select name="category">
+	  <option value="personal" {$selected['personal']}>personal</option>
+	  <option value="school" {$selected['school']}>school</option>
+	  <option value="work" {$selected['work']}>work</option>
+	  <option value="uncategorized" {$selected['uncategorized']}>uncategorized</option>
+  </select>
+  </p>
+
+  <p>Title<br />
+  <input type="text" name="title" value="$title" placeholder="title" maxlength="255" size="80"></p>
+
+  <p>Description<br />
+  <textarea name="description" rows="6" cols="80" placeholder="description">$description</textarea></p>
+  <input type="submit" name='submit' value="Submit"> <input type="submit" name='cancel' value="Cancel">
+</form>
+</body>
+</html>
+EOT2;
+
+			print $html;
 		}
 		
-		public function addTask($data) {
-			$this->error = '';
+		public function errorView($message) {
+			$body = "<h1>Tasks</h1>\n";
+			$body .= "<p>$message</p>\n";
 			
-			$title = $data['title'];
-			$category = $data['category'];
-			$description = $data['description'];
-			
-			if (! $title) {
-				$this->error = "No title found for task to add. A title is required.";
-				return $this->error;			
-			}
-			
-			if (! $category) {
-				$category = 'uncategorized';
-			}
-			
-			$titleEscaped = $this->mysqli->real_escape_string($title);		
-			$categoryEscaped = $this->mysqli->real_escape_string($category);
-			$descriptionEscaped = $this->mysqli->real_escape_string($description);
-	
-			$sql = "INSERT INTO tasks (title, description, category, addDate) VALUES ('$titleEscaped', '$descriptionEscaped', '$categoryEscaped', NOW())";
-	
-			if (! $result = $this->mysqli->query($sql)) {
-				$this->error = $this->mysqli->error;
-			}
-			
-			return $this->error;
+			return $this->page($body);
 		}
 		
-		public function updateTaskCompletionStatus($id, $status) {
-			$this->error = "";
-		
-			$completedDate = 'null';
-			if ($status == 'completed') {
-				$completedDate = 'NOW()';
-			}
-	
-			if (!$id) {
-				$this->error = "No task was specified to change completion status.";
-			} else {
-				$idEscaped = $this->mysqli->real_escape_string($id);
-				$sql = "UPDATE tasks SET completedDate = $completedDate WHERE id = '$idEscaped'";
-				if (! $result = $this->mysqli->query($sql) ) {
-					$this->error = $this->mysqli->error;
-				}
-			}
-	
-			return $this->error;
-		}
-		
-		public function updateTask($data) {
-			$this->error = '';
-			
-			if (! $this->mysqli) {
-				$this->error = "No connection to database.";
-				return $this->error;
-			}
-			
-			$id = $data['id'];
-			if (! $id) {
-				$this->error = "No id specified for task to update.";
-				return $this->error;			
-			}
-			
-			$title = $data['title'];
-			if (! $title) {
-				$this->error = "No title found for task to update. A title is required.";
-				return $this->error;			
-			}		
-			
-			$description = $data['description'];
-			$category = $data['category'];
-			
-			$idEscaped = $this->mysqli->real_escape_string($id);
-			$titleEscaped = $this->mysqli->real_escape_string($title);
-			$descriptionEscaped = $this->mysqli->real_escape_string($description);
-			$categoryEscaped = $this->mysqli->real_escape_string($category);
-			$sql = "UPDATE tasks SET title='$titleEscaped', description='$descriptionEscaped', category='$categoryEscaped' WHERE id = $idEscaped";
-			if (! $result = $this->mysqli->query($sql) ) {
-				$this->error = $this->mysqli->error;
-			} 
-			
-			return $this->error;
-		}
-		
-		public function deleteTask($id) {
-			$this->error = '';
-			
-			if (! $this->mysqli) {
-				$this->error = "No connection to database.";
-				return $this->error;
-			}
-			
-			if (! $id) {
-				$this->error = "No id specified for task to delete.";
-				return $this->error;			
-			}			
-		
-			$idEscaped = $this->mysqli->real_escape_string($id);
-			$sql = "DELETE FROM tasks WHERE id = $idEscaped";
-			if (! $result = $this->mysqli->query($sql) ) {
-				$this->error = $this->mysqli->error;
-			}
-			
-			return $this->error;
+		private function page($body) {
+			$html = <<<EOT
+<!DOCTYPE html>
+<html>
+<head>
+<title>{$this->pageTitle}</title>
+<link rel="stylesheet" type="text/css" href="{$this->stylesheet}">
+</head>
+<body>
+$body
+<p>&copy; 2017 Dale Musser. All rights reserved.</p>
+</body>
+</html>
+EOT;
+			return $html;
 		}
 
-	
-	}
-
-?>
+}
